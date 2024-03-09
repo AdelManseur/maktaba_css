b1205491a7b8bc75fb3c2cbd64528b111d0fea59
AdelManseur <153110085+AdelManseur@users.noreply.github.com>
2024-03-09 14:41:20 +0100

    o

--- addToMainCSS.bat
+++ addToMainCSS.bat
@@ -1,35 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Get the latest file in the current directory
-set "latestFile="
-set "latestTimestamp=0"
-
-for %%F in (*) do (
-    set "file=%%F"
-    
-    REM Get the last modified timestamp of the file
-    for /f "tokens=1-3 delims= " %%a in ('dir /TW /OD "%%F" ^| findstr "^[0-9]"') do (
-        set "timestamp=%%a"
-        set "time=%%b"
-        set "ampm=%%c"
-        
-        REM Convert the timestamp to a sortable format
-        set "sortableTimestamp=!timestamp!!time!"
-        set "sortableTimestamp=!sortableTimestamp: =0!"
-        
-        REM Compare timestamps to find the latest file
-        if !sortableTimestamp! gtr !latestTimestamp! (
-            set "latestTimestamp=!sortableTimestamp!"
-            set "latestFile=!file!"
-        )
-    )
-)
+set /p latestFILE=<latestFile.txt
 
 REM Save the latest file name to a file
-
-    echo @import url('%latestFile%'); >> main.css
+if defined latestFile (
+    echo @import url('%latestFILE%') >> main.css
     echo File saved successfully.
+) else (
+    echo No files found in the current directory.
+)
 
 
 echo Press 'a' to execute the Gulp command.
